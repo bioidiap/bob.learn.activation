02934f8a3312d827827c6588c17a217f548790fe
Andre Anjos <andre.anjos@idiap.ch>
2014-01-15 12:04:39 +0100

    Provide get_include()

--- xbob/learn/activation/__init__.py
+++ xbob/learn/activation/__init__.py
@@ -1,2 +1,7 @@
 from ._library import *
 from ._library import __version__, __api_version__
+
+def get_include():
+  """Returns the directory containing the C/C++ API include directives"""
+
+  return __import__('pkg_resources').resource_filename(__name__, 'include')
